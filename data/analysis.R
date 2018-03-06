14ee3c23520fa1632e444785a8bf146e0e3d9f67
Josh Alley <jkalley14@tamu.edu>
2018-03-06 09:52:43 -0600

    change to lagged DV specification

--- data/analysis.R
+++ data/analysis.R
@@ -9,7 +9,6 @@ library(dplyr)
 library(rstan)
 library(bayesplot)
 library(shinystan)
-library(loo)
 library(reshape2)
 library(party)
 library(xtable)
@@ -32,30 +31,32 @@ options(mc.cores = parallel::detectCores())
 
 # Define a state-year level dataset with no missing observations
 reg.state.data <- state.char %>%
-  select(ccode, year, change.ln.milex, 
+  select(ccode, year, ln.milex, lag.ln.milex,
                       atwar, civilwar, rival.mil, ln.GDP, polity, majpower)
 
 # Add state membership in alliances to this data
 reg.state.data <- left_join(reg.state.data, state.mem)
 
 # Replace missing alliance values with zero 
-reg.state.data[, 10: 354][is.na(reg.state.data[, 10: 354])] <- 0
+reg.state.data[, 11: 355][is.na(reg.state.data[, 11: 355])] <- 0
 
 # Remove observations with missing values
 reg.state.comp <- reg.state.data[complete.cases(reg.state.data), ]
 
 # Rescale the state-level regressors
- reg.state.comp[, 4:9] <- lapply(reg.state.comp[, 4:9], 
+ reg.state.comp[, 4:10] <- lapply(reg.state.comp[, 4:10], 
        function(x) rescale(x, binary.inputs = "0/1"))
 
-
+ 
 # Check the range and distribution of the DV
-summary(reg.state.comp$change.ln.milex)
-sd(reg.state.comp$change.ln.milex)
-plot(density(reg.state.comp$change.ln.milex))
+summary(reg.state.comp$ln.milex)
+sd(reg.state.comp$ln.milex)
+ggplot(reg.state.comp, aes(ln.milex)) + geom_density()
+
+
 
 # Create a matrix of state membership in alliances (Z in STAN model)
-state.mem.mat <- as.matrix(reg.state.comp[, 10: 354])
+state.mem.mat <- as.matrix(reg.state.comp[, 11: 355])
 
 # create a state index variable
 reg.state.comp$state.id <- reg.state.comp %>% group_indices(ccode)
@@ -66,7 +67,7 @@ reg.state.comp$year.id <- reg.state.comp %>% group_indices(year)
 
 # Create the matrix of alliance-level variables
 reg.all.data <- alliance.char %>%
-  select(atopid, prob.det, uncond.det, compellent, bilat, armsreq, dem.prop, wartime, consul.only)
+  select(atopid, prob.det, uncond.det, compellent, bilat, armsreq, dem.prop, wartime, onlyconsul)
 
 # Create an alliance index variable
 alliance.id <- reg.all.data %>% group_indices(atopid)
@@ -82,7 +83,7 @@ reg.all.data <- reg.all.data[complete.cases(reg.all.data), ]
 
 ### transform data into matrices for STAN
 # State-level characeristics
-reg.state.mat <- as.matrix(reg.state.comp[, 4:9])
+reg.state.mat <- as.matrix(reg.state.comp[, 4:10])
 
 # check correlations among state-level regressors
 cor(reg.state.mat, method = "pearson")
@@ -97,8 +98,6 @@ cor(alliance.reg.mat, method = "pearson")
 # Add a constant to the alliance-level regression 
 cons <- rep(1, length = nrow(alliance.reg.mat))
 alliance.reg.mat <- cbind(cons, alliance.reg.mat)
-
-alliance.reg.mat.sub <- alliance.reg.mat[1:10, ]
 
 
 
@@ -116,32 +115,35 @@ stan.data <- list(N = nrow(reg.state.comp), y = reg.state.comp[, 3],
 # Compile the model code for the variational Bayes algorithm
 model.1 <- stan_model(file = "data/multi-member ML model.stan")
 
-# Variational Bayes- use to check coefficients on model
+# Variational Bayes- use to check coefficients and posterior predictions on model
 ml.model.vb <- vb(model.1, data = stan.data, seed = 12)
 launch_shinystan(ml.model.vb)
 
+# posterior predictive check from variational Bayes- did not converge
+# so treat these predictions with caution
+y = reg.state.comp[, 3]
+vb.model.sum <- extract(ml.model.vb)
+ppc_dens_overlay(y, vb.model.sum$y_pred[1:100, ])
 
 
  
 # Regular STAN
 system.time(
   ml.model <- stan(file = "data/multi-member ML model.stan", data = stan.data, 
-                       iter = 1000, warmup = 500, chains = 4, cores = 4
+                       iter = 2000, warmup = 1000, chains = 4, cores = 4
   )
 )
 
 # diagnose full model
 launch_shinystan(ml.model)
 
-check_energy(ml.model)
-check_divergences(ml.model)
-check_treedepth(ml.model)
+check_hmc_diagnostics(ml.model)
 
 # Traceplots for the regression parameters
 traceplot(ml.model, pars = "beta")
 traceplot(ml.model, pars = "gamma")
 
-# Pairs plots to see sources of autocorrelation in the chains
+# Pairs plots to see sources of autocorrelation in the chains: Select only some intercept parameters
 # Check for correlation between the means and variances of the state intercepts
 pairs(ml.model, pars = c("alpha", "sigma_state", "alpha_state[1]", "alpha_state[2]", "alpha_state[15]", 
                              "alpha_state[26]"))
@@ -155,13 +157,27 @@ pairs(ml.model, pars = c("alpha", "sigma_year", "alpha_year[2]", "alpha_year[14]
 pairs(ml.model, pars = c("alpha", "alpha_state[2]", "alpha_year[14]", "alpha_state[25]", 
                          "alpha_year[35]"))
 
+# Check correlation among the parameters
+# Alliance-level variables
+pairs(ml.model, pars = c("beta[1]", "beta[2]", "beta[3]", "beta[4]",
+                         "beta[5]", "beta[6]", "beta[7]", "beta[8]"))
+
+# State-level variables
+pairs(ml.model, pars = c("gamma[1]", "gamma[2]", "gamma[3]", "gamma[4]",
+                         "gamma[5]", "gamma[6]", "gamma[7]"))
 
 
 # Extract coefficients from the model
 ml.model.sum <- extract(ml.model, permuted = TRUE)
 
-# Because the varying intercepts were modeling with a non-centered parameterization. need to process them further for interpretation.
-# 
+# Posterior predictive distributions relative to observed data
+yrep.full <- ml.model.sum$y_pred
+
+# plot posterior predictive denisty of first 50 simulations
+ppc_dens_overlay(y, yrep.full[1:100, ])
+
+
+
 
 
 # Plot all the beta coefficients and calculate posterior probabilities
@@ -170,7 +186,7 @@ colnames(ml.model.sum$beta) <- colnames(alliance.reg.mat)
 
 
 mean(ml.model.sum$beta[, 2] > 0) # posterior probability that a probabilistic alliance is associated with increased defense spending
-mean(ml.model.sum$beta[, 3] < 0) # posterior probability that an unconditional deterrent alliance is associated with increased spending
+mean(ml.model.sum$beta[, 3] < 0) # posterior probability that an unconditional deterrent alliance is associated with decreased spending
 mean(ml.model.sum$beta[, 4] > 0) # Posterior probability that a compellent alliance is associated with increased spending
 mean(ml.model.sum$beta[, 5] > 0) # Posterior probability that a bilateral alliance is associated with increased spending
 mean(ml.model.sum$beta[, 6] > 0) # Posterior probability that a arms requirements in an alliance are associated with increased spending
@@ -182,7 +198,7 @@ beta.melt <- melt(ml.model.sum$beta)
 # Plot density of the coefficients
 ggplot(beta.melt, aes(x=value, fill = Var2)) +
   geom_density(alpha=0.25) +
-  ggtitle("Posterior Distribution of Alliance-Level Covariates")
+  ggtitle("Posterior Distribution of Alliance-Level Regression Coefficients")
 
 # Summarize intervals
 beta.summary <- summary(ml.model, pars = c("beta", "sigma_all"), probs = c(0.05, 0.95))$summary
@@ -190,7 +206,7 @@ rownames(beta.summary) <- c("Constant", "Probabilistic Deterrent", "Unconditiona
                             "Compellent", "Bilateral", "Arms Requirement", "Share Dem. Members", 
                             "Wartime", "sigma Alliances")
 print(beta.summary)
-xtable(beta.summary)
+xtable(beta.summary, digits = 3)
 
 
 # Similar calculations for the state-level variables
@@ -198,12 +214,13 @@ xtable(beta.summary)
 colnames(ml.model.sum$gamma) <- colnames(reg.state.mat)
 
 # posterior probabilities
-mean(ml.model.sum$gamma[, 1] > 0) # posterior probability that being at war leads to increased defense spending
-mean(ml.model.sum$gamma[, 2] > 0) # posterior probability that civil wars lead to increased defense spending
-mean(ml.model.sum$gamma[, 3] > 0) # posterior probability that rival military expenditures are associated with increased spending
-mean(ml.model.sum$gamma[, 4] > 0) # Posterior probability that GDP is associated with increased spending
-mean(ml.model.sum$gamma[, 5] < 0) # posterior probability that democracies tend to decrease spending
-mean(ml.model.sum$gamma[, 6] > 0) # posterior probability that major powers increase spending
+mean(ml.model.sum$gamma[, 1] > 0) # posterior probability that lagged DV is positive
+mean(ml.model.sum$gamma[, 2] > 0) # posterior probability that being at war leads to increased defense spending
+mean(ml.model.sum$gamma[, 3] > 0) # posterior probability that civil wars lead to increased defense spending
+mean(ml.model.sum$gamma[, 4] > 0) # posterior probability that rival military expenditures are associated with increased spending
+mean(ml.model.sum$gamma[, 5] > 0) # Posterior probability that GDP is associated with increased spending
+mean(ml.model.sum$gamma[, 6] < 0) # posterior probability that democracies tend to decrease spending
+mean(ml.model.sum$gamma[, 7] > 0) # posterior probability that major powers increase spending
 
 gamma.melt <- melt(ml.model.sum$gamma)
 
@@ -215,7 +232,7 @@ ggplot(gamma.melt, aes(x=value,  fill = Var2)) +
 
 # summarize posterior intervals
 gamma.summary <- summary(ml.model, pars = c("gamma", "sigma_state", "alpha"), probs = c(0.05, 0.95))$summary
-rownames(gamma.summary) <- c("Wartime", "Civil War", "Rival Mil. Expenditure", 
+rownames(gamma.summary) <- c("Lagged Expenditures", "Wartime", "Civil War", "Rival Mil. Expenditure", 
                             "ln(GDP)", "Polity", "Major Power", "Sigma State", "Constant")
 print(gamma.summary)
 xtable(gamma.summary)
@@ -234,7 +251,8 @@ coef.probs <- as.data.frame(append(beta.probs, gamma.probs))
 colnames(coef.probs) <- c("Posterior Probability of Positive Coefficient")
 rownames(coef.probs) <- c("Alliance Model Constant", "Probabilistic Deterrent", "Unconditional Deterrent", 
                           "Compellent", "Bilateral", "Arms Requirement", "Share Dem. Members", 
-                          "Wartime Alliance",  "State War", "Civil War", "Rival Mil. Expenditure", 
+                          "Lag Expenditures",
+                          "Wartime Alliance",  "Interstate War", "Civil War", "Rival Mil. Expenditure", 
                             "ln(GDP)", "Polity", "Major Power")
 coef.probs$variable <- rownames(coef.probs)
 coef.probs$variable <- reorder(coef.probs$variable, coef.probs$`Posterior Probability of Positive Coefficient`)
@@ -243,8 +261,7 @@ coef.probs$variable <- reorder(coef.probs$variable, coef.probs$`Posterior Probab
 ggplot(coef.probs, aes(x = variable, y = `Posterior Probability of Positive Coefficient`)) + 
   geom_col() +
   geom_text(aes(label = `Posterior Probability of Positive Coefficient`), nudge_y = .05) +
-  coord_flip() +
-  ggtitle("Posterior Probability of Positive Regression Coefficient")
+  coord_flip()
 ggsave("figures/post-prob.png", height = 6, width = 8)
 
 
